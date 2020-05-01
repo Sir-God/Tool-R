5495948929de4381d5f4c47da62a2e32d869f20b
SirGod <sir.god@hotmail.com>
2020-05-02 01:09:11 +0530

    fix a error

--- tool/ports.py
+++ tool/ports.py
@@ -7,19 +7,19 @@ GREEN = Fore.GREEN
 RESET = Fore.RESET
 GRAY = Fore.LIGHTBLACK_EX
 
+def is_port_open(host, port):
+	s = socket.socket()
+	try:
+		s.connect((host, port))
+	except:
+		return False
+	else:
+		return True
+		
 def portSc():
-	banner()
-	def is_port_open(host, port):
-		s = socket.socket()
-		try:
-			s.connect((host, port))
-		except:
-			return False
-		else:
-			return True
-		host = input(f"  {GREEN}Enter the host:{RESET}")
-		for port in range(1, 1025):
-			if is_port_open(host, port):
-				print(f"{GREEN}[+] {host}:{port} is open      {RESET}")
-		else:
-			print(f"{GRAY}[!] {host}:{port} is closed    {RESET}", end="\r")
+	host = input(f"  {GREEN}Enter the host:{RESET}")
+	for port in range(1, 1025):
+		if is_port_open(host, port):
+			print(f"{GREEN}[+] {host}:{port} is open      {RESET}")
+	else:
+		print(f"{GRAY}[!] {host}:{port} is closed    {RESET}", end="\r")
