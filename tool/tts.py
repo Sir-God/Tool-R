c6463acee8675128973b941a12143cf6b0aec151
SirGod <sir.god@hotmail.com>
2020-05-01 16:34:06 +0530

    upgrade style in tts

--- tool/tts.py
+++ tool/tts.py
@@ -9,11 +9,12 @@ RC, GC, YC, CC, DF = '\033[1;31m', '\033[1;32m', '\033[1;33m', '\033[1;36m', '\0
 
 def textspeech():
 	banner()
-	ttst = input("input text for create them mp3: ")
-	ttsn = input("Enter name for output mp3 with .mp3: ")
+	ttst = input(f"{GC}input text for create Text to speech mp3 file \n  {YC}>>>{DF} ")
+	ttsn = input(f"\n{GC}Enter name for output mp3 with .mp3 \n   {YC}>>>{DF} ")
 
 	tts = gtts.gTTS(ttst, lang='en')
 
 	tts.save(ttsn)
 	os.system('play-audio' + ' ' + ttsn)
-	print('audio file save in current directory as ',ttsn)
+	print(f'{RC}audio file save in current directory as{DF} ',ttsn)
+	sleep(5)
