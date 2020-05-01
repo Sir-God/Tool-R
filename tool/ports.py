d393b36c535c3a7acc7eb4391078a669841c6b58
SirGod <sir.god@hotmail.com>
2020-05-02 01:59:58 +0530

    fix a error

--- tool/ports.py
+++ tool/ports.py
@@ -11,16 +11,23 @@ def is_port_open(host, port):
 	s = socket.socket()
 	try:
 		s.connect((host, port))
+		s.settimeout(0.2)
 	except:
 		return False
 	else:
 		return True
-		
+	
 def portSc():
 	banner()
+	print(f"            {GREEN}PORT SCANNER{RESET}\n\n")
 	host = input(f"  {GREEN}Enter the host:{RESET}")
-	for port in range(1, 65335):
+	print(f"\n          {GRAY}now select Range For Ports{RESET}")
+	port1 = input('input Starting Port : ')
+	port2 = input('input end port : ')
+	port1 = int(port1)
+	port2 = int(port2)
+	for port in range(port1, port2):
 		if is_port_open(host, port):
 			print(f"  {GREEN}[+] {host}:{port} is open      {RESET}")
-	else:
-		print(f"  {GRAY}[!] {host}:{port} is closed    {RESET}", end="\r")
+		else:
+			print(f"  {GRAY}[!] {host}:{port} is closed    {RESET}", end="\r")
